43bdf6562615c4811f513c14e3f6fe86d02286b9
admin <neba.nfonsang@dataiku.com>
2023-09-05 12:32:24 -0600

    Edited files

--- custom-recipes/data-partitioning/recipe.py
+++ custom-recipes/data-partitioning/recipe.py
@@ -89,12 +89,23 @@ def write_partitions_timestamp(df, partition):
         output_folder.upload_stream(file_name, data)
 
 # partition the dataset and write partitions to the managed folder
-i=0
-for partition_df in partition_dfs:
+if partitioning_column:
+    i=0
+    for partition_df in partition_dfs:
+        partition = partition_values[i]
+        if include_timestamp:
+            write_partitions_timestamp(partition_df, partition)
+        else:
+            write_partitions(partition_df, partition)
+        i=i+1
+        logging.info("Finished writing CSV files to the folder")
+else:
     partition = partition_values[i]
-    if include_timestamp:
-        write_partitions_timestamp(partition_df, partition)
-    else:
-        write_partitions(partition_df, partition)
-    i=i+1
-    logging.info("Finished writing CSV files to the folder")
+        if include_timestamp:
+            write_partitions_timestamp(partition_df, partition)
+        else:
+            write_partitions(partition_df, partition)
+        i=i+1
+        logging.info("Finished writing CSV files to the folder")
+
+    
