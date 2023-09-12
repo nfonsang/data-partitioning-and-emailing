4869c7ff8e55b4b87827bc23ee219ce79275bd11
admin <neba.nfonsang@dataiku.com>
2023-09-12 16:28:09 -0600

    Edited files

--- custom-recipes/data-partitioning/recipe.py
+++ custom-recipes/data-partitioning/recipe.py
@@ -110,6 +110,7 @@ def write_partitions(df, partition):
     if partitioning_columns:
         if file_format=="excel":
             file_name = f"{partition}.xlsx"
+            df=df.applymap(str)
             with io.BytesIO() as buf:
                 df.to_excel(buf, sheet_name=sheet_name, encoding='utf-8', index = None, header = True)
                 output_folder.upload_stream(file_name, buf.getvalue())
