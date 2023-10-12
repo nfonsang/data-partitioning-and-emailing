6afb14d903f16d2121943433068cef85f436d0de
admin <neba.nfonsang@dataiku.com>
2023-10-12 15:06:35 -0600

    Edited files

--- custom-recipes/data-partitioning/recipe.py
+++ custom-recipes/data-partitioning/recipe.py
@@ -6,6 +6,11 @@ import datetime
 import logging
 import io
 
+# remove borders on header 
+import pandas.io.formats.excel
+pandas.io.formats.excel.ExcelFormatter.header_style = None
+
+
 from dataiku.customrecipe import get_input_names_for_role
 from dataiku.customrecipe import get_output_names_for_role
 from dataiku.customrecipe import get_recipe_config
