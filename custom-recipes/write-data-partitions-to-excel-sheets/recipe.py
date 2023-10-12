4e016b3a009373a3ed2f36fe6591cf1f699d5ff2
admin <neba.nfonsang@dataiku.com>
2023-10-12 12:41:01 -0600

    Edited files

--- custom-recipes/write-data-partitions-to-excel-sheets/recipe.py
+++ custom-recipes/write-data-partitions-to-excel-sheets/recipe.py
@@ -126,7 +126,7 @@ def write_partitions():
             if use_partition_value_for_sheetname:
                 with io.BytesIO() as buf:
                     df.to_excel(buf, sheet_name=final_sheet_names[i], startrow=start_row, startcol=start_col, encoding='utf-8', index = None, header = True)
-                    output_folder.upload_stream(excel_name, buf.getvalue())
+            output_folder.upload_stream(excel_name, buf.getvalue())
             
                 #dframe.to_excel(writer, sheet_name=final_sheet_names[i], 
                                     #startrow=start_row, startcol=start_col, 
